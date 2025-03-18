e41c2464925d5b0d854dd612335ef88557d0a19a
Alok Prajapati <113538469+Alok3333@users.noreply.github.com>
2025-03-18 23:47:00 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -29,7 +29,6 @@ const Login = () => {
 
     try {
       let res = await fetch("https://x-login-sign-up.vercel.app/auth/login", {
-        mode: 'no-cors',
         method: "POST",
         headers: {
           "Content-Type": "application/json",
